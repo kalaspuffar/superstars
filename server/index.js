9974cff8ff2ac4c3cb3ef8fb6f72cd15ab0c0824
Daniel Persson <mailto.woden@gmail.com>
2017-05-17 15:16:39 +0200

    Started with login procedure.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var bodyParser = require('body-parser')
+
+var profiles = {};
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -8,16 +11,39 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/users/:userId/messages', function (req, res) {
-   fs.readFile( __dirname + "/" + "messages.json", 'utf8', function (err, data) {
-       res.end( data );
-   });
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+
+app.post('/login', function (req, res) {
+  const crypto = require('crypto');
+  const secret = 'abcdefg';
+  const hash = crypto.createHmac('sha256', secret)
+                     .update(req.body.pass)
+                     .digest('hex');
+  fs.readFile( __dirname + "/" + "profiles.json", 'utf8', function (err, data) {
+    var jsonData = JSON.parse(data);
+    for(var i in jsonData) {
+      if(jsonData[i].username == req.body.name && jsonData[i].password == hash) {
+        res.end( '{"status": "logged in"}' );
+      }
+    }
+    res.end( '{"status": "Wrong credentials"}' );
+  });
 })
 
-app.get('/users/:userId', function (req, res) {
-   fs.readFile( __dirname + "/" + "profiles.json", 'utf8', function (err, data) {
-       res.end( data );
-   });
+app.get('/messages', function (req, res) {
+  fs.readFile( __dirname + "/" + "messages.json", 'utf8', function (err, data) {
+   res.end( data );
+  });
+})
+
+app.get('/profile', function (req, res) {
+  fs.readFile( __dirname + "/" + "profiles.json", 'utf8', function (err, data) {
+    res.end( data );
+  });
 })
 
 var server = app.listen(3000, function () {
